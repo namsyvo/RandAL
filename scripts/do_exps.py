ad1353e81a09aad4479ea626f96ade96b948b143
Nam S Vo <nsvo@delphinus.cs.memphis.edu>
2015-09-04 16:35:46 -0500

    Update documents. Update experiment scripts.

--- scripts/do_exps.py
+++ scripts/do_exps.py
@@ -15,8 +15,8 @@ def cal_paras(length, error_prob, att_para, th_para):
 	minws = int(math.ceil(length/(th + 1))) + 3
 	return th, att, minws
 
-dir_names = ['Dros', 'Ecoli', 'Pseu', 'Stap', 'Strep', 'Soran']
-rlen = [100]
+dir_names = ['Drosophila3R.fasta', 'Staphylococcus.fasta']
+rlen = [35, 51, 76, 100, 200, 400]
 
 for d in dir_names:
 	if not os.path.isdir(d):
@@ -26,10 +26,10 @@ for d in dir_names:
 			os.mkdir(d + '/' + str(length))
 
 		th, att, minws = cal_paras(length, 0.02, 1, 4)
-		B_file = '../dataset/' + d + '/B_100_index'
-		F_file = '../dataset/' + d + '/F_100_index'
-		ref_file = '../dataset/' + d + '/' + d
-		read_file = '../dataset/' + d + '/' + d + '-' + str(length) + '.fq'
+		B_file = '../data/genomes/' + d + '/B_100_index'
+		F_file = '../data/genomes/' + d + '/F_100_index'
+		ref_file = '../data/genomes/' + d + '/' + d
+		read_file = '../data/reads/' + d + '/' + d + '-' + str(length) + '.fq'
 		result_path = d + '/' + str(length) +'/results/'
 		if not os.path.isdir(result_path):
 			os.mkdir(result_path)
@@ -39,7 +39,7 @@ for d in dir_names:
 			os.mkdir(eval_path)
 		sam_file = result_path + d + '-' + str(length) + '-' + str(th) + '_' + str(att) + '_' + str(minws) + '.sam'
 		time_file = eval_path + 'all_time.txt'
-		exp_cmd = '(time ./randomized ' + B_file + ' ' + F_file + ' ' + ref_file + ' ' + read_file + ' ' + sam_file + \
+		exp_cmd = '(time ./randal-align ' + B_file + ' ' + F_file + ' ' + ref_file + ' ' + read_file + ' ' + sam_file + \
 						 ' -th ' + str(th) + ' -att ' + str(att) + ' -mode 3 -rep 10 -minws ' + str(minws) + \
 						 ' -maxcdt 2)' + ' 2>>' + time_file + ' &'
 		os.system(exp_cmd)
